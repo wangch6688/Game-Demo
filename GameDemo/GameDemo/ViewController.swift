2704d66983269f8f7ec95369339a044ff5742ca4
Kevin <wangch6688@hotmail.com>
2018-07-02 09:05:43 +0800

    updated

--- GameDemo/GameDemo/ViewController.swift
+++ GameDemo/GameDemo/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
+let KScreenWidth = UIScreen.main.bounds.size.width
+let KScreenHeight = UIScreen.main.bounds.size.height
+
+
 class ViewController: UIViewController {
+    
+    var startGameBtn : UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        defaultSetting()
+    }
+    
+    func defaultSetting() {
+        view.backgroundColor = UIColor.black
+        
+        startGameBtn = UIButton.init(frame: CGRect(x: KScreenWidth/4, y: KScreenHeight - 60, width: KScreenWidth/2, height: 52))
+        startGameBtn?.setTitle("Start Game", for: .normal)
+        startGameBtn?.backgroundColor = UIColor.red
+        startGameBtn?.layer.cornerRadius = 5
+        startGameBtn?.layer.masksToBounds = true
+        startGameBtn?.addTarget(self, action: #selector(startButtonAction), for: .touchUpInside)
+        view.addSubview(startGameBtn!)
+        
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    @objc func startButtonAction() {
+//        arc4random(
+        let number = arc4random() % 360
+        print("-------------\(number)--------")
+//        startGameBtn?.isHidden = true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
 }
 
